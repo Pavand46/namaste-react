7c016a172fa8dfabfecc321e57f622d642ede798
Pavand46 <pavankalyandasari1234@gmail.com>
2024-09-13 11:13:17 +0530

    Update App.js
    Wrote code for creating nested elements inside React

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-const heading = React.createElement(
-  "h1",
-  { id: "header" },
-  "Hello world from React"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "header" },
+//   "Hello world from React"
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// console.log(heading); //Object
+
+// Creating a nested HTML Structure
+
+/*
+
+<div id = "parent">
+
+  <div id = "child">
+
+    <h1>I am inside h1 tag</h1>
+
+  <div>
+
+  <div id = "child">
+
+    <h1>I am inside h1 tag</h1>
+
+  <div>
+
+<div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am inside h1 tag"),
+    React.createElement("h2", {}, "I am inside h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am inside h1 tag"),
+    React.createElement("h2", {}, "I am inside h2 tag"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
-console.log(heading); //Object
+root.render(parent);
